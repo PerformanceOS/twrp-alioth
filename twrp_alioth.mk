852263173a880c59f6bdb127cc53e8dc2d5e7358
johnmart19 <johnivan19999@gmail.com>
2022-05-22 16:50:28 +0300

    alioth: Initial TWRP Adaptation

--- twrp_alioth.mk
+++ twrp_alioth.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common PixelPlusUI stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 WITH_GAPPS := true
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_BOOT_ANIMATION_RES := 1440
@@ -30,7 +30,7 @@ TARGET_SUPPORTS_QUICK_TAP  := true
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := aosp_alioth
+PRODUCT_NAME := twrp_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
